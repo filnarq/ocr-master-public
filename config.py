cb050c11084c181ec0e248e286f878b73a6b8754
filnarq <herka03@gmail.com>
2024-11-21 15:07:32 +0200

    Fix

--- config.py
+++ config.py
@@ -6,25 +6,25 @@ cfg = edict()
 cfg.train = edict()
 cfg.train.dataset_dir = "data/skewed"
 cfg.train.dataset_resize = 24
-cfg.train.batch_size = 48
+cfg.train.batch_size = 32
 cfg.train.epochs = 20
-cfg.train.lr = 0.05
-cfg.train.momentum = 0.75
-cfg.train.model_path = 'pths/skewed_r_24_b_62_e_20_lr_005_m_09_5e.pth'
-cfg.save_every_nth_epoch = 5
-cfg.print_loss_every_n_batches = 450
+cfg.train.lr = 0.04
+cfg.train.momentum = 0.6
+cfg.train.model_path = 'pths/skewed_2.pth'
+cfg.train.save_every_nth_epoch = 5
+cfg.train.print_loss_every_n_batches = 450
 
 # Test
 cfg.test = edict()
 cfg.test.dataset_dir = "data/skewed"
 cfg.test.dataset_resize = 24
 cfg.test.batch_size = 48
-cfg.test.model_path = 'pths/skewed_r_24_b_62_e_20_lr_005_m_09_5e.pth'
+cfg.test.model_path = 'pths/skewed_2.pth'
 
 # Inference
 cfg.inference = edict()
 cfg.inference.image_path = 'test.jpg'
-cfg.inference.model_path = 'pths/skewed_r_24_b_62_e_20_lr_005_m_09_5e.pth'
+cfg.inference.model_path = 'pths/skewed_2.pth'
 cfg.inference.classes = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
                          'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
                          'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
--- train.py
+++ train.py
@@ -9,17 +9,20 @@ from load_dataset import imshow
 class Net(nn.Module):
     def __init__(self):
         super().__init__()
-        self.conv1 = nn.Conv2d(3, 18, 5)
+        self.conv1 = nn.Conv2d(3, 18, 3)
         self.pool1 = nn.MaxPool2d(2, 2)
-        self.conv2 = nn.Conv2d(18, 48, 3)
+        self.conv2 = nn.Conv2d(18, 48, 2)
         self.pool2 = nn.MaxPool2d(2, 2)
-        self.fc1 = nn.Linear(48 * 4 * 4, 256)
+        self.conv3 = nn.Conv2d(48, 64, 2)
+        self.pool3 = nn.MaxPool2d(2, 2)
+        self.fc1 = nn.Linear(64 * 2 * 2, 256)
         self.fc2 = nn.Linear(256, 128)
         self.fc3 = nn.Linear(128, 62)
 
     def forward(self, x):
         x = self.pool1(F.relu(self.conv1(x)))
         x = self.pool2(F.relu(self.conv2(x)))
+        x = self.pool3(F.relu(self.conv3(x)))
         x = torch.flatten(x, 1) # flatten all dimensions except batch
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
